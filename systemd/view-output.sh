f8641a9333d84943ad53781164dd4d485d6838b3
Ben Kennish <ben@kennish.net>
2026-03-04 19:32:57 +0000

    See colours in view-output

--- systemd/view-output.sh
+++ systemd/view-output.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo journalctl -u auto-vc-status -n 200 -f -o cat
+# -n 200 : show 200 lines
+# -f     : follow output
+# -o cat : plain raw output
+
+sudo journalctl -u auto-vc-status -o cat -n 200 -f
